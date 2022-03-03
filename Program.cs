27f3d47b1c9e05f9d46586223ad556530da1f513
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-03 18:44:41 -0300

    terminei a licao

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Aranha
+{
+    class p
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(@" \\()// ");
+            Console.WriteLine(@"//(__)\\");
+            Console.WriteLine("||    ||");
+        }
+    }
+}
+Console.ResetColor();
