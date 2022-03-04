164c4cd6bd14f413216e77b0a18fdf959c93c00d
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-04 20:47:29 -0300

    TERMINEI A LICAO

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
 ﻿
-namespace Aranha
-{
-    class p
-    {
-        static void Main(string[] args)
-        {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(" ||  || ");
             Console.WriteLine(@" \\()// ");
             Console.WriteLine(@"//(__)\\");
             Console.WriteLine("||    ||");
-        }
-    }
-}
-Console.ResetColor();
+            Console.ResetColor();
